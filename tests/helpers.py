bce75a713c264ca37ca05c19198374e40f2ae45d
erik-whiting <erik@erikwhiting.com>
2019-09-26 20:17:48 -0500

    Rename test helper to helper

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,6 +2,6 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def test_element_text(element, text):
+def evaluate_element_text(element, text):
 	WebDriverWait(element.driver, 10).until(ec.text_to_be_present_in_element(element.locator, text))
 	return element.text == text
