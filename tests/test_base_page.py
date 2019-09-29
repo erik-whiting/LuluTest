57d568ccb32dfe5d49af708e8343107d90f121e6
erik-whiting <erik@erikwhiting.com>
2019-09-28 21:54:08 -0500

    Update tests to be more robust

--- tests/test_base_page.py
+++ tests/test_base_page.py
@@ -12,6 +12,16 @@ class TestBasePage(unittest.TestCase):
 		bp = BasePage.Page(self.cf)
 		self.assertEqual(bp.url, self.cf.url())
 
-	def test_bast_page_returns_config_url_with_sub_dir(self):
+	def test_base_page_returns_config_url_with_sub_dir(self):
 		bp = BasePage.Page(self.cf, 'about')
 		self.assertEqual(bp.url, self.cf.url() + '/about')
+
+	def test_page_knows_its_headless(self):
+		self.cf.options_list = ['headless']
+		bp = BasePage.Page(self.cf)
+		self.assertIn("headless", bp.options_list)
+
+	def test_page_knows_its_not_headless(self):
+		self.cf.options_list = []
+		bp = BasePage.Page(self.cf)
+		self.assertNotIn("headless", bp.options_list)
--- tests/test_feature.py
+++ tests/test_feature.py
@@ -9,10 +9,20 @@ class TestFeature(unittest.TestCase):
 	cf.base_url = 'erikwhiting.com'
 	cf.subdomain = ''
 	cf.base_url += '/newsOutlet'
-	bp = BasePage.Page(cf)
 
-	def test_write_and_click(self):
-		bp = self.bp
+	def test_write_and_click_with_headless(self):
+		self.cf.options_list = ["headless"]
+		bp = BasePage.Page(self.cf)
+		bp.go()
+		bp.element_by("id", "sourceNews").input_text("Hello")
+		bp.element_by("id", "transmitter").click()
+		english_div = helper.evaluate_element_text(bp.element_by("id", "en1"), "Hello")
+		self.assertTrue(english_div)
+		bp.close()
+
+	def test_write_and_click_without_headless(self):
+		self.cf.options_list = []
+		bp = BasePage.Page(self.cf)
 		bp.go()
 		bp.element_by("id", "sourceNews").input_text("Hello")
 		bp.element_by("id", "transmitter").click()
