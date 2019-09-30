a3fc3501594505a249c610c08e97861a9e7d0171
erik-whiting <erik@erikwhiting.com>
2019-09-29 23:31:29 -0500

    Add test class for other pages

--- tests/test_other_pages.py
+++ tests/test_other_pages.py
@@ -1,10 +1,20 @@
 import unittest
+from configs import config
+from page import page
 
 
-class MyTestCase(unittest.TestCase):
-	def test_something(self):
-		self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-	unittest.main()
+class TestOtherPages(unittest.TestCase):
+	def test_calculator_page(self):
+		cf = config.Config()
+		cf.http_prefix = 'http://'
+		cf.base_url = 'www.math.com/'
+		cf.base_url += 'students/calculators/source/basic.htm'
+		bp = page.Page(cf)
+		bp.go()
+		bp.element_by("name", "five").click()
+		bp.element_by("name", "plus").click()
+		bp.element_by("name", "seven").click()
+		bp.element_by("name", "DoIt").click()
+		answer = bp.element_by("name", "Input").get("value")
+		self.assertEqual(answer, "12")
+		bp.close()
