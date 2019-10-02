1d5b5c3638bc68b5aaaa78fb9237bb967902f342
alwinaind <alwinaind@gmail.com>
2019-10-02 11:45:05 +0530

    Update step.py
    Added functionality to tell what the step is doing

--- page/step.py
+++ page/step.py
@@ -3,3 +3,8 @@ class Step:
 		self.action = action
 		self.element = element
 		self.data = data
+	def explain(self):
+    		if('a' in self.element.element.tag_name or 'e' in self.element.element.tag_name or 'i' in self.element.element.tag_name or 'o' in self.element.element.tag_name or 'u' in self.element.element.tag_name)
+			print("This step {0} {1} into an {2} element of {3} {4}".format(self.action, self.data,self.element.element.tag_name, self.element.by, self.element.value))
+		else:
+			print("This step {0} {1} into a {2} element of {3} {4}".format(self.action, self.data,self.element.element.tag_name, self.element.by, self.element.value))
