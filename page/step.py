f8d6ac20a5df35cb4e8e27fa2738e62292a5ff91
erik-whiting <erik@erikwhiting.com>
2019-10-04 17:33:02 -0500

    Cleanup step class a little

--- page/step.py
+++ page/step.py
@@ -3,13 +3,18 @@ class Step:
 		self.action = action
 		self.element = element
 		self.data = data
-		self.__tag_name = ''
+		self.tag_name = ''
+
 	def tag_name_setter(self):
-		if isinstance(type(self.element),str):
+		if isinstance(type(self.element), str):
 			self.tag_name = ''
 		else:
 			self.tag_name = self.element.element.tag_name
 			
 	def explain(self):
 		self.tag_name_setter()
-		return str("This step {0} {1} into a {2} element of {3} {4}".format(self.action, self.data, self.tag_name, self.element.by, self.element.value))
+		return str(
+			"This step {0} {1} into a {2} element of {3} {4}".format(
+				self.action, self.data, self.tag_name, self.element.by, self.element.value
+			)
+		)
