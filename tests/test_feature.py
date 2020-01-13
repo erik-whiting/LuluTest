dd244972d56c9638a40f4cee6f09a9416e29dbba
erik-whiting <erik@erikwhiting.com>
2020-01-12 18:20:48 -0600

    Update test_feature test script

--- tests/test_feature.py
+++ tests/test_feature.py
@@ -1,84 +1,39 @@
 import unittest
 
-from page.page import Page
-from action.action import Action
-from element import *
-from step.old_step import Step
-from tests import helpers as helper
+from page import Page
+from element import PageElement
+from action import Action
 
 
 class TestFeature(unittest.TestCase):
-    page = Page('erikwhiting.com/newsOutlet')
-    actions = Action()
+    page = Page('http://erikwhiting.com/newsOutlet')
     page.elements = [
         PageElement(("id", "sourceNews"), "input box"),
         PageElement(("id", "transmitter"), "button"),
         PageElement(("id", "en1"), "english div")
     ]
 
-    def test_write_and_click_with_headless(self):
+    def setUp(self):
+        self.actions = Action()
+        self.actions.go(self.page)
 
-        self.bp.element("input box").input_text("Hello")
-        self.bp.element("button").click()
-        english_div = helper.evaluate_element_text(self.bp.element("english div"), "Hello")
-        self.assertTrue(english_div)
+    def tearDown(self):
+        self.actions.close()
+        del self.actions
 
     def test_page_source_feature(self):
-        source = self.bp.page_source()
+        source = self.actions.get_page_source()
         self.assertIn('<body onload="defaultBreaking()">', source)
 
-    def test_page_url(self):
-        current_url = self.bp.get_url()
-        if current_url[current_url.__len__() - 1] == "/":
-            current_url = current_url[:-1]
-        self.assertEqual(self.cf.url(), current_url)
-
     def test_page_refresh(self):
-        self.bp.refresh()
-        current_url = self.bp.get_url()
-        if current_url[current_url.__len__() - 1] == "/":
-            current_url = current_url[:-1]
-        self.assertEqual(self.cf.url(), current_url)
+        self.actions.refresh()
+        current_url = self.actions.get_url()
+        self.assertEqual('http://erikwhiting.com/newsOutlet/', current_url)
 
     def test_page_change_url(self):
-        current_url = self.bp.get_url()
-        if current_url[current_url.__len__() - 1] == "/":
-            current_url = current_url[:-1]
-        self.assertEqual(self.cf.url(), current_url)
+        current_url = self.actions.get_url()
+        self.assertEqual('http://erikwhiting.com/newsOutlet/', current_url)
 
-        self.bp.navigate_to('https://github.com')
-        current_url = self.bp.get_url()
-        if current_url[current_url.__len__() - 1] == "/":
-            current_url = current_url[:-1]
-        self.assertEqual('https://github.com', current_url)
-
-    def test_do(self):
-        self.bp.collect_elements([
-            ("id", "sourceNews"),
-            ("id", "transmitter")
-        ])
-        input_element = self.bp.elements[0]
-        transmit_button = self.bp.elements[1]
-        steps = [
-            Step("type", input_element, "Hello"),
-            Step("click", transmit_button)
-        ]
-        self.bp.do(steps)
-        english_div = helper.evaluate_element_text(
-            self.bp.grab("id", "en1"),
-            "Hello"
-        )
-        self.assertTrue(english_div)
-
-    def test_named_elements(self):
-        self.bp.collect_elements([
-            ("id", "sourceNews", "input"),
-            ("id", "transmitter", "button")
-        ])
-        steps = [
-            Step("type", self.bp.element("input"), "Hello"),
-            Step("click", self.bp.element("button"))
-        ]
-        self.bp.do(steps)
-        english_div = helper.evaluate_element_text(self.bp.grab("id", "en1"), "Hello")
-        self.assertTrue(english_div)
+        self.actions.go_to('https://github.com')
+        current_url = self.actions.get_url()
+        self.assertEqual('https://github.com/', current_url)
