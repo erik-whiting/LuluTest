2dfae94c4269dff5e1b0dcc197f6fa5d0d0f6061
erik-whiting <erik@erikwhiting.com>
2020-05-03 23:41:00 -0500

    Add decoration to PageActions class

--- action/page_actions_decorator.py
+++ action/page_actions_decorator.py
@@ -1,6 +1,12 @@
 from action.page_actions import PageActions
 from lulu_exceptions import PageNotLoadedError
 
+'''
+This class contains meta-programming to decorate the
+PageActions class methods with the necessary pre and
+post processing functionality. Proceed with caution.
+'''
+
 
 class PageActionsDecorator(object):
     CHECK_PAGE_LOAD = [
@@ -15,25 +21,26 @@ class PageActionsDecorator(object):
         'close'
     ]
 
-    def __init__(self, page_actions: PageActions):
-        self.model = page_actions
+    def __init__(self, model: PageActions):
+        self.model = model
 
-    def __getattr__(self, item):
-        func = getattr(self.model, item)
-        if item in self.CHECK_PAGE_LOAD:
-            self.check_page_load(func)
-        elif item in self.SET_PAGE_LOAD:
-            self.set_page_loaded(func)
+    def __getattr__(self, func):
+        def method(*args):
+            if func in self.CHECK_PAGE_LOAD:
+                return self.check_page_load(func, *args)
+            if func in self.SET_PAGE_LOAD:
+                return self.set_page_loaded(func, *args)
+        return method
 
-    def check_page_load(self, func):
+    def check_page_load(self, func, *args):
         if not self.model.page_loaded:
             raise PageNotLoadedError("Web Driver must go to page before performing this action")
         else:
-            func()
+            return getattr(self.model, func)(*args)
 
-    def set_page_loaded(self, func):
-        func()
-        if func.__name__ == 'close':
+    def set_page_loaded(self, func, *args):
+        getattr(self.model, func)(*args)
+        if func == 'close':
             self.model.page_loaded = False
         else:
             self.model.page_loaded = True
