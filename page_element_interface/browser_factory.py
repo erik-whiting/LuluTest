e2901c833a1e5b66e825c05e7d1612adcc718787
erik-whiting <erik@erikwhiting.com>
2020-07-01 19:01:48 -0500

    Use webdriver_manager in browser factory

--- page_element_interface/browser_factory.py
+++ page_element_interface/browser_factory.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 def new(browser_type, options):
@@ -11,13 +13,15 @@ def new(browser_type, options):
 
 def __chrome_driver(options):
     chrome_options = webdriver.chrome.options.Options()
+    cdm = ChromeDriverManager().install()
     if 'headless' in options:
         chrome_options.add_argument('--headless')
-    return webdriver.Chrome(options=chrome_options)
+    return webdriver.Chrome(cdm, options=chrome_options)
 
 
 def __firefox_driver(options):
     firefox_options = Options()
+    gdm = GeckoDriverManager().install()
     if 'headless' in options:
         firefox_options.headless = True
-    return webdriver.Firefox(options=firefox_options)
+    return webdriver.Firefox(executable_path=gdm, options=firefox_options)
