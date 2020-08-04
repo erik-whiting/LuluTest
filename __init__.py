7e4965f562b1d5197a470722ad3b07a94ced94f1
erik-whiting <erik@erikwhiting.com>
2020-08-04 00:08:43 -0500

    import all in root level __init__

--- __init__.py
+++ __init__.py
@@ -1,18 +1,6 @@
-from action.action import Action
-from action.page_actions import PageActions
-from action.element_actions import ElementActions
-
-from element.base_element import BaseElement
-from element.page_element import PageElement
-from element.alert_element import AlertElement
-from element.locator import Locator
-
-from lulu_exceptions.LuluExceptions import *
-
-from page.page import Page
-
-from page_element_interface import IPageElement
-
-from step.step import Step
-from step.steps import Steps
-from step.do import Do, DoStep
+from .action import *
+from .element import *
+from .lulu_exceptions import *
+from .page import *
+from .page_element_interface import *
+from .step import *
