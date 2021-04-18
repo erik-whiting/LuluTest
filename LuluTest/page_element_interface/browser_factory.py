e18fb90a1474bd947f8bc892af61963c80bf8967
erik-whiting <erik@erikwhiting.com>
2021-04-18 14:57:49 -0500

    Add MS Edge to browser factory

--- LuluTest/page_element_interface/browser_factory.py
+++ LuluTest/page_element_interface/browser_factory.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
+from msedge.selenium_tools import EdgeOptions
+from msedge.selenium_tools import webdriver as EdgeDriver
 
 
 def new(browser_type, options):
@@ -21,3 +23,11 @@ def __firefox_driver(options):
     if 'not headless' not in options:
         firefox_options.headless = True
     return webdriver.Firefox(options=firefox_options)
+
+
+def __edge_driver(options):
+    edge_options = EdgeOptions()
+    edge_options.use_chromium = True
+    if 'not headless' not in options:
+        edge_options.add_argument('headless')
+    return EdgeDriver.WebDriver(options=edge_options)
