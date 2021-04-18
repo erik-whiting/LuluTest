70df6c5103db010a224a331c3c26827833890e07
erik-whiting <erik@erikwhiting.com>
2021-04-17 19:35:24 -0500

    Version number bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='LuluTest',
-    version='1.1.0',
+    version='1.1.1',
     author='Erik Whiting',
     author_email='erik@erikwhiting.com',
     description='A web browser automation framework',
