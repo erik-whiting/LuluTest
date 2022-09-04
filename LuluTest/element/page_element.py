0234ff30f08bf192867a4772f5b947ac64f2f508
erik-whiting <erik@erikwhiting.com>
2022-09-04 17:52:40 -0500

    Use properties for element type indicators

--- LuluTest/element/page_element.py
+++ LuluTest/element/page_element.py
@@ -5,4 +5,21 @@ class PageElement(BaseElement):
     def __init__(self, locator, name=''):
         super().__init__(name)
         self.locator = locator
-        self.is_page_element = True
+
+    @property
+    def is_page_element(self):
+      return True
+
+    @is_page_element.setter
+    def is_page_element(self, *args, **kwargs):
+      # This property cannot be changed
+      return False
+
+    @property
+    def is_alert_element(self):
+      return False
+
+    @is_alert_element.setter
+    def is_alert_element(self, *args, **kwargs):
+      # This property cannot be changed
+      return False
