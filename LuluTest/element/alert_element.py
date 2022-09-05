87f2552e8db933ee29edfa16fbaea66b698825a5
[Ujjwal Mahar] <[ujjwal.mahar01@gmail.com]>
2022-09-05 23:32:25 +0530

    I made some changes

--- LuluTest/element/alert_element.py
+++ LuluTest/element/alert_element.py
@@ -1,7 +1,24 @@
-from LuluTest.element import BaseElement
+from LuluTest.element import BaseElement, PageElement
 
 
 class AlertElement(BaseElement):
     def __init__(self, name=''):
         super().__init__(name)
         self.is_alert_element = True
+    @property
+    def is_page_element(self):
+      return True
+
+    @is_page_element.setter
+    def is_page_element(self, *args, **kwargs):
+      # This property cannot be changed
+      return False
+
+    @property
+    def is_alert_element(self):
+      return False
+
+    @is_alert_element.setter
+    def is_alert_element(self, *args, **kwargs):
+      # This property cannot be changed
+      return False
